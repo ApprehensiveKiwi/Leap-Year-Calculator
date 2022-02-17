330a4fd34936f3ad55a1ddada5a946655ed565e1
Max Okunev <max.okunev@education.nsw.gov.au>
2022-02-17 17:38:47 +1100

    working

--- leapyearsprojectyessir/Program.cs
+++ leapyearsprojectyessir/Program.cs
@@ -5,16 +5,15 @@ int year = Convert.ToInt32(Console.ReadLine());
 int x = 0;
 foreach (int value in Enumerable.Range(year, 4*rep+4))
 {
+    if (x == rep)
+    {
+        break;
+    }
     if ((value % 400 == 0))
     {
         if ((value % 100) == 0)
             Console.WriteLine(x + ": " + value + " is a leap year");
-             x = x + 1;
-        if (x == rep)
-        {
-            break;
-
-        }
+             x = x + 100;
     }
     else if ((value % 4 == 0))
     {
@@ -22,10 +21,5 @@ foreach (int value in Enumerable.Range(year, 4*rep+4))
         { 
             Console.WriteLine(x + ": " + value + " is a leap year");
         x = x + 1;
-        if (x == rep)
-        {
-            break;
-        }
     }
     }
-}
