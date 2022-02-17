019995d5943db7d38931b77514e2c8acf68674c6
max.okunev <max.okunev@BORGER>
2022-02-17 17:23:32 +1100

    Add project files.

---
+++ leapyearsprojectyessir/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Number of leap years to be found: ");
+int rep = Convert.ToInt32(Console.ReadLine())+1;
+Console.WriteLine("Year to start from: ");
+int year = Convert.ToInt32(Console.ReadLine());
+int x = 0;
+foreach (int value in Enumerable.Range(year, 4*rep+4))
+{
+    if ((value % 400 == 0))
+    {
+        if ((value % 100) == 0)
+            Console.WriteLine(x + ": " + value + " is a leap year");
+             x = x + 1;
+        if (x == rep)
+        {
+            break;
+
+        }
+    }
+    else if ((value % 4 == 0))
+    {
+        if ((value % 100 != 0))
+        { 
+            Console.WriteLine(x + ": " + value + " is a leap year");
+        x = x + 1;
+        if (x == rep)
+        {
+            break;
+        }
+    }
+    }
+}
